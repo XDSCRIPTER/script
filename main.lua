88992303fb101743a14a1e4b715ec998a3218891
XDSCRIPTER <lagfag053@gmail.com>
2025-12-24 03:17:48 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -64,7 +64,7 @@ local katargetcountdropdown = Tabs.Combat:CreateDropdown("katargetcountdropdown"
 local kaswingcooldownslider = Tabs.Combat:CreateSlider("kaswingcooldownslider", { Title = "Attack Cooldown (s)", Min = 0.01, Max = 1.01, Rounding = 2, Default = 0.1 })
 local AutoHealToggle = Tabs.Combat:CreateToggle("AutoHealToggle", { Title = "Auto Heal", Default = false })
 local HealPercent = Tabs.Combat:CreateSlider("HealPercent", { Title = "Heal at", Min = 1, Max = 100, Rounding = 2, Default = 0.1 })
-local HealFruitDropDown = Tabs.Combat:CreateDropdown("HealFruitDropDown", {Title = "Select Fruit to eat",Values = {"Bloodfruit", "Bluefruit", "Lemon", "Coconut", "Jelly", "Banana", "Orange", "Oddberry", "Berry", "Strangefruit", "Strawberry", "Sunjfruit", "Pumpkin", "Prickly Pear", "Apple",  "Barley", "Cloudberry", "Carrot"}, Default = "Bloodfruit"})
+local HealFruitDropDown = Tabs.Farming:CreateDropdown("HealFruitDropDown", {Title = "Select Fruit to eat",Values = {"Bloodfruit", "Bluefruit", "Lemon", "Coconut", "Jelly", "Banana", "Orange", "Oddberry", "Berry", "Strangefruit", "Strawberry", "Sunjfruit", "Pumpkin", "Prickly Pear", "Apple",  "Barley", "Cloudberry", "Carrot"}, Default = "Bloodfruit"})
 --{MAP TAB}     
 
 local resourceauratoggle = Tabs.Map:CreateToggle("resourceauratoggle", { Title = "Resource Aura", Default = false })
@@ -189,9 +189,17 @@ local function swingtool(tspmogngicl)
     end
 end
 
-local function Eating(SelectED)
-    if packets.UseBagItem and packets.UseBagItem.send then
-        packets.UseBagItem.send(SelectED)
+local function Eating(itemname)
+
+    local inventory = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.RightPanel.Inventory:FindFirstChild("List")
+    if not inventory then return end
+
+    for _, child in ipairs(inventory:GetChildren()) do
+        if child:IsA("ImageLabel") and child.Name == itemname then
+           if packets.UseBagItem and packets.UseBagItem.send then
+               packets.UseBagItem.send(itemname)
+           end
+       end
     end
 end
 
